abb1260ca15526859c6af873f438deb62d0a0e2e
Eren Taşçı <erent4sci@gmail.com>
2024-04-08 01:06:40 +0300

    feat:reusable presentation component

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,20 +1,12 @@
-import Image from "next/image";
-import homeImg from "../../public/home.jpg";
+import Hero from "@/components/hero";
+import heroImg from "../../public/home.jpg";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-      <div className="absolute -z-10 inset-0">
-        <Image
-          src={homeImg}
-          alt="car factory"
-          fill
-          style={{
-            objectFit: "cover",
-          }}
-        />
-      </div>
-    </div>
+    <Hero
+      imgData={heroImg}
+      imgAlt="car factory"
+      title="Proffesional Cloud Hosting"
+    />
   );
 }
