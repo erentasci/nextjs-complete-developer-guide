84f7326338e1b2fdcc439797b01eb9b6f6e6f429
Eren Taşçı <erent4sci@gmail.com>
2024-04-09 23:47:59 +0300

    feat: create snippet form

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,3 +1,32 @@
 export default function SnippetCreatePage() {
-  return <div>Snippet Create Page</div>;
+  return (
+    <form>
+      <h3 className="font-bold m-3"> Create a snippet </h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            className="border rounded p-2 w-full"
+            name="title"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            className="border rounded p-2 w-full"
+            name="code"
+            id="code"
+          />
+        </div>
+        <button className="bg-blue-300 text-white p-2 rounded" type="submit">
+          Create Snippet
+        </button>
+      </div>
+    </form>
+  );
 }
