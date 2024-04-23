8bc17af8c6c2aabf2ae1d56cc915057092aaa9b0
Eren Taşçı <erent4sci@gmail.com>
2024-04-23 14:14:06 +0300

    feat: understanding nextjs caching system

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { db } from "@/db";
 
@@ -9,15 +10,16 @@ export async function editSnippet(id: number, code: string) {
     data: { code },
   });
 
+  revalidatePath(`/snippets/${id}`);
   redirect(`/snippets/${id}`);
 }
-
 
 export async function deleteSnippet(id: number) {
   await db.snippet.delete({
     where: { id },
   });
 
+  revalidatePath("/");
   redirect("/");
 }
 
@@ -27,39 +29,39 @@ export async function createSnippet(
   },
   formData: FormData
 ) {
- try {
-   const title = formData.get("title");
-   const code = formData.get("code");
+  try {
+    const title = formData.get("title");
+    const code = formData.get("code");
 
-   if (typeof title !== "string" || title.length < 3) {
-     return {
-       message: "Title must be longer",
-     };
-   }
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
 
-   if (typeof code !== "string" || code.length < 10) {
-     return {
-       message: "Code must be longer",
-     };
-   }
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
 
-   await db.snippet.create({
-     data: {
-       title,
-       code,
-     },
-   });
- } catch (error: unknown) {
-   if (error instanceof Error) {
-     return {
-       message: error.message,
-     };
-   } else {
-     return {
-       message: "Something went wrong...",
-     };
-   }
- }
-
- redirect("/");
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        message: error.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
+  }
+  revalidatePath("/");
+  redirect("/");
 }
