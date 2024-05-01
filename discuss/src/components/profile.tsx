f4bdc2b07c65069323f8188700b31a485834806c
Eren Taşçı <erent4sci@gmail.com>
2024-05-01 17:28:23 +0300

    feat: sign in-out, auth status

---
+++ discuss/src/components/profile.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default function Profile() {
+  const session = useSession();
+
+  if (session.data?.user) {
+    return (
+      <div>From client:{JSON.stringify(session.data.user)} is signed in</div>
+    );
+  }
+
+  return <div>From client: user NOT signed in</div>;
+}
