f679d54e29f57677081174f58c4641437f2b2c70
Eren Taşçı <erent4sci@gmail.com>
2024-05-03 12:10:32 +0300

    feat: building the header

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,22 +1,7 @@
-import * as actions from "@/actions";
 import { auth } from "@/auth";
-import Profile from "@/components/profile";
-import { Button } from "@nextui-org/react";
 
 export default async function Home() {
   const session = await auth();
 
-  return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign in!</Button>
-      </form>
-
-      <form action={actions.signOut}>
-        <Button type="submit">Sign out!</Button>
-      </form>
-
-      <Profile />
-    </div>
-  );
+  return <div></div>;
 }
