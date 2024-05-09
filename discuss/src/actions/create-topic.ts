c752db4046c4e72fa9e7dcb147febd3cbd76644e
Eren Taşçı <erent4sci@gmail.com>
2024-05-09 23:24:32 +0300

    feat: popup form, zod

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,7 +1,25 @@
 "use server";
 
-export async function createTopic(){
+import { z } from "zod";
 
+const createTopicSchema = z.object({
+    name: z.string().min(3).regex(/^[a-z-]+$/, {
+        message: "Must be lowercase letters without spaces" 
+    }),
+    description: z.string().min(10)
+})
+
+export async function createTopic(formData: FormData){
     // TODO: revalidate the homepage 
 
+   const result = createTopicSchema.safeParse({
+    name : formData.get('name'),
+    description : formData.get('description')
+   })
+
+   if(!result.success){
+    console.log(result.error.flatten().fieldErrors);
+    
+   }
+    
 }
--- discuss/src/components/topics/topic-create-form.tsx
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,40 @@
+import * as actions from "@/actions";
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Textarea,
+} from "@nextui-org/react";
+
 export default function TopicCreateForm() {
-  return <div>Topic Create Form</div>;
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form
+          action={actions.createTopic}
+          className="flex flex-col p-4 gap-4 w-80"
+        >
+          <h3 className="text-xl">Create a Topic</h3>
+          <Input
+            name="name"
+            labelPlacement="outside"
+            label="Name"
+            placeholder="Name"
+          />
+          <Textarea
+            name="description"
+            labelPlacement="outside"
+            label="Description"
+            placeholder="Describe your Topic"
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 }
