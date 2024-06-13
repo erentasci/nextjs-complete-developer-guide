ca74dbc3713fca10ac02c3e1e68b7507fff479db
Eren Taşçı <erent4sci@gmail.com>
2024-06-13 10:52:18 +0300

    feat: passing additional args to a server action

--- discuss/src/actions/create-post.ts
+++ discuss/src/actions/create-post.ts
@@ -1,45 +1,61 @@
 "use server";
 
 import { auth } from "@/auth";
+import { db } from "@/db";
 import { z } from "zod";
 
 const createPostSchema = z.object({
-    title: z.string().min(3),
-    content: z.string().min(10)
-})
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
 
 interface CreatePostFormState {
-    errors:{
-        title?:string[],
-        content?:string[],
-        _form?:string[]
-    }
-};
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
 
+export async function createPost(
+  slug: string,
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
 
-export async function createPost(formState:CreatePostFormState,formData:FormData) : Promise<CreatePostFormState>{
-    const result = createPostSchema.safeParse({
-        title: formData.get('title'),
-        content: formData.get('content')
-    })
-    
-    if(!result.success){
-        return {
-            errors:result.error.flatten().fieldErrors
-        }
-    }
-
-    const session = await auth();
-
-    if(!session || !session.user) {
-        return {
-            errors:{
-                _form: ["You must be signed in to do this"],
-            }
-        }
-    }
-
+  if (!result.success) {
     return {
-        errors:{}
-    }
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug },
+  });
+
+  if(!topic) {
+    return {
+        errors: {
+          _form: ["Cannot find topic"],
+        },
+      };
+  }
+
+  return {
+    errors: {},
+  };
 }
