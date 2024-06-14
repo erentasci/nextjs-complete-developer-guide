801b06c86c8978a8a6b127ccfc1a1a70a59fb4a8
Eren Taşçı <erent4sci@gmail.com>
2024-06-14 15:38:04 +0300

    feat: final

--- discuss/src/components/header.tsx
+++ discuss/src/components/header.tsx
@@ -6,6 +6,7 @@ import {
   NavbarItem,
 } from "@nextui-org/react";
 import Link from "next/link";
+import { Suspense } from "react";
 import SearchInput from "./search-input";
 
 export default function Header() {
@@ -18,7 +19,9 @@ export default function Header() {
       </NavbarBrand>
       <NavbarContent justify="center">
         <NavbarItem>
-          <SearchInput />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
         </NavbarItem>
       </NavbarContent>
 
